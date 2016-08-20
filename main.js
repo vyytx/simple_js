41afb5abd1ab9561635650e3c7b61d8b97675324
z-vyytx <dobeender@gmail.com>
2016-08-20 22:30:12 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-
+function sjs(TF)){
+  if (TF==true&&TF=="true"){
+   @ssss
+  }else if(TF==false&&TF=="false"){
+    alert('You need to make it "true" to enable "Simple Javascript"');
+  }else{
+    alert('The currect instruction is "sjs(true/false)"')
+  }
+}
