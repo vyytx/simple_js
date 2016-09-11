7a59980838d6ad55f20619d49dd2e8c9fc9bf6ce
z-vyytx <dobeender@gmail.com>
2016-09-11 21:44:37 +0800

    Update sj.js

--- sj.js
+++ sj.js
@@ -7,10 +7,10 @@ var sjConf ={
   sjBol : ""
 }
 
-$("body").append("<sjr>Powered by SimpleJS</sjr>")
-
 var sj = {
   say: function(text){if(text !== "help"){alert(text);}else{alert(SJ.sjCU + '"sj.say(' + SJ.sjStr + '/' + SJ.sjNum +'/' + SJ.sjBol + ');"' )}},
   question: function(text){if(text !== "help"){prompt(text);}else{alert('正確的使用方法為 "sj.question(文字);"')}},
   textNum: function(text){if(text !== help){return text/1}else{alert('正確的使用方法為 "sj.textNum(文字);"')}}
 }
+
+setTimeout($("body").append("<sjr>Powered by SimpleJS</sjr>"), 1000);
