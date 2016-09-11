523482f9c48863a95b430d448827b9d0e0bcfbee
z-vyytx <dobeender@gmail.com>
2016-09-11 21:51:37 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,4 +4,4 @@ imported1.src = 'https://vyytx.github.io/Charge-App/js/jquery.js';
 imported2.src = 'https://vyytx.github.io/simple_js/sj.js';
 document.head.appendChild(imported1);
 document.head.appendChild(imported2);
-document.head.appendChild(document.createElement("script").appendChild(document.createTextNode(setTimeout($("body").append("<sjr>Powered by SimpleJS</sjr>"), 1000))));
+setTimeout(document.body.appendChild(document.createElement("sjr").appendChild(document.createTextNode("Powered by SimpleJS"), 1000)));
