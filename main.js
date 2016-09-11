98d5ed82af38b0426f3c04d1ce5f2e41492eb243
z-vyytx <dobeender@gmail.com>
2016-09-11 21:22:42 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,5 +4,3 @@ imported1.src = 'https://vyytx.github.io/Charge-App/js/jquery.js';
 imported2.src = 'https://vyytx.github.io/simple_js/sj.js';
 document.head.appendChild(imported1);
 document.head.appendChild(imported2);
-
-$("body").append("<sjr>使用了SimpleJS<sjr>")
