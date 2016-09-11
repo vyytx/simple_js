4b24049b3da4898e70da1b80558bec58b8eeb5da
z-vyytx <dobeender@gmail.com>
2016-09-11 20:39:38 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 var sj = {
   say: function(text){if(text !== undefind){alert(text);}else{alert('正確的使用方法為 "sj.say(文字/數字/布林值);"')}},
   question: function(text){if(text !== undefind){prompt(text);}else{alert('正確的使用方法為 "sj.question(文字);"')}},
-  Is = {
-    number: function(text){return text/1}
-  }
+  textNumber: function(text){return text/1}
 }
