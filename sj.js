4ab096f0a420bfaee6fde6d927a473debe895aef
z-vyytx <dobeender@gmail.com>
2016-09-11 21:13:59 +0800

    Update sj.js

--- sj.js
+++ sj.js
@@ -1,5 +1,5 @@
 var sj = {
   say: function(text){if(text !== "help"){alert(text);}else{alert('正確的使用方法為 "sj.say(文字/數字/布林值);"')}},
   question: function(text){if(text !== "help"){prompt(text);}else{alert('正確的使用方法為 "sj.question(文字);"')}},
-  textNumber: function(text){return text/1}
+  textNum: function(text){if(text !== help){return text/1}else{alert('正確的使用方法為 "sj.textNum(文字);"')}}
 }
