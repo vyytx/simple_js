9eee13b1cd3600747f061048345f6a0203d38f4a
z-vyytx <dobeender@gmail.com>
2016-09-11 20:33:57 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,7 @@
-var sjsT = false;
-function sjs(TF){
-  if (TF==true&&TF=="true"){
-    sjsT = true;
-  }else if(TF==false&&TF=="false"){
-    alert('你需要把它設為"true"來啟動"Simple Javascript"');
-  }else{
-    alert('正確的使用方法為 "sjs(true/false)"')
-  }
-}
-function _(){
-  if(sjsT = true){
-    function say(text){
-      if (text !== undefind || text !== ""){
-        alert(text);
-      }else{
-        alert('正確的使用方法為 "say(文字)"')
-      }
-    }
-    
-    //fr
-    say();
-    
+var sj = {
+  say: function(text){if(text !== undefind){alert(text);}else{alert('正確的使用方法為 "sj.say(文字/數字/布林值);"')}},
+  question: function(text){if(text !== undefind){prompt(text);}else{alert('正確的使用方法為 "sj.question(文字);"'}},
+  Is = {
+    number: function(text){return text/1}
   }
 }
