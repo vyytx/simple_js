05cee7a734ffb1cbd3e5a6b9e61b73ec66b148eb
z-vyytx <dobeender@gmail.com>
2016-09-12 20:45:22 +0800

    Update sj.js

--- sj.js
+++ sj.js
@@ -1,4 +1,4 @@
-var sjConf ={
+var SJ ={
   sjSet : function(lang){if(lang == "en-us"){this.sjLang="en-us"; this.sjCU="The correct instruction is "; this.sjStr="String"; this.sjNum="Number"; this.sjBol="Boolean";}else if(lang == "zh-tw"){this.sjLang="zh-tw"; this.sjCU="正確的使用方式是 "; this.sjStr="字串"; this.sjNum="數字"; this.sjBol="布林值";}},
   sjLang : "en-us",
   sjCU : "",
@@ -9,7 +9,7 @@ var sjConf ={
 
 document.body.appendChild(document.createElement('sb'));
 document.getElementsByTagName("sb")[0].id = "sb";
-document.getElementById("sb").appendChild(document.createTextNode("CLICK ME"));
+document.getElementById("sb").appendChild(document.createTextNode("Powered by SimpleJS"));
 
 var sj = {
   say: function(text){if(text !== "help"){alert(text);}else{alert(SJ.sjCU + '"sj.say(' + SJ.sjStr + '/' + SJ.sjNum +'/' + SJ.sjBol + ');"' )}},
