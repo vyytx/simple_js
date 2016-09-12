bffd5aeae6a7c42e91aa793deb453fbee01a6988
z-vyytx <dobeender@gmail.com>
2016-09-12 19:03:41 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,10 +4,3 @@ imported1.src = 'https://vyytx.github.io/Charge-App/js/jquery.js';
 imported2.src = 'https://vyytx.github.io/simple_js/sj.js';
 document.head.appendChild(imported1);
 document.head.appendChild(imported2);
-
-var sj_para = document.createElement("sjr");
-var sj_node = document.createTextNode("Powered by SimpleJS");
-sj_para.appendChild(sj_node);
-var sj_body = document.getElementsByTagName("body"); 
-setTimeout(11);
-sj_body[0].appendChild(sj_para);
