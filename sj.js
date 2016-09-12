ec037e2575db617165c69ee8e1a58910a19c25bb
z-vyytx <dobeender@gmail.com>
2016-09-12 22:25:56 +0800

    Update sj.js

--- sj.js
+++ sj.js
@@ -11,9 +11,7 @@ document.body.appendChild(document.createElement('br'));
 document.body.appendChild(document.createElement('sb'));
 document.body.appendChild(document.createElement('img'));
 
-var __oo__ = document.getElementsByTagName("img");
-var ___oo___ = document.getElementsByTagName("img").length - 1;
-__oo__[___oo___].src("https://vyytx.github.io/simple_js/img.png");
+document.getElementsByTagName("img")[document.getElementsByTagName("img") - 1].src = "https://vyytx.github.io/simple_js/img.png";
 document.getElementById("sbimg").style = "text-align: center; display: block; margin: 0 auto;";
 
 document.getElementsByTagName("sb")[0].id = "sb";
