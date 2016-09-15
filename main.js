eda4952ad9de46beca7c2a6ec10c06aa813c1899
z-vyytx <dobeender@gmail.com>
2016-09-15 20:01:08 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
 var imported1 = document.createElement('script');
-var imported2 = document.createElement('script');
-imported1.src = 'https://vyytx.github.io/Charge-App/js/jquery.js';
-imported2.src = 'https://vyytx.github.io/simple_js/sj.js';
+imported1.src = 'https://vyytx.github.io/simple_js/sj.js';
 document.head.appendChild(imported1);
-document.head.appendChild(imported2);
