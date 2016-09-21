36745a1554614a156c95237bcacafe14c8b6a931
z-vyytx <dobeender@gmail.com>
2016-09-21 16:27:23 +0800

    Update sj.js

--- sj.js
+++ sj.js
@@ -11,31 +11,29 @@ var SJ={
 }
 
 function sjSet(langu, IsBS){
-  var ___i = langu;
-  var ___ii = IsBS;
-  if(___i == "en_US"){
+  if(langu == "en_US"){
     SJ.sjLang="en_US";
     SJ.sjCU="The correct instruction is ";
     SJ.sjStr="String"; 
     SJ.sjNum="Number";
     SJ.sjBol="Boolean";
     
-    if(___ii != "true"){
+    if(IsBS != "true"){
       onloadSJ(1);
-    }else if(___ii == "true"){
+    }else if(IsBS == "true"){
       $("#SJModal").remove();
       $("body").append('<!-- Modal --><div class="modal fade" id="SJModal" role="dialog"><div class="modal-dialog"><!-- Modal content--><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button><h4 class="modal-title">SimpleJS</h4></div><div class="modal-body"><div id="SJsb"><h3>This website is Powered by SimpleJS.</h3><h3>You can get SimpleJS <a href="">here</a> .</h3></div></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div>')
       $("#SJModal").modal.show();
-    }else if(___i == "zh_TW"){
+    }else if(langu == "zh_TW"){
       SJ.sjLang="zh_TW";
       SJ.sjCU="正確的使用方式是 ";
       SJ.sjStr="字串";
       SJ.sjNum="數字";
       SJ.sjBol="布林值";
       
-        if(___ii != "true"){
+        if(IsBS != "true"){
           onloadSJ(1);
-        }else if(___ii == "true"){
+        }else if(IsBS == "true"){
           $("#SJModal").remove();
           $("body").append('<!-- Modal --><div class="modal fade" id="SJModal" role="dialog"><div class="modal-dialog"><!-- Modal content--><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button><h4 class="modal-title">SimpleJS</h4></div><div class="modal-body"><div id="SJsb"><h3>這個網站使用了SimpleJS,</h3><h3>您可以點擊<a href="">這裡</a>來下載SimpleJS</h3></div></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">關閉</button></div></div></div></div>')
           $("#SJModal").modal.show;
