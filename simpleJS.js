7464cca15677cbe04e1dbba218f812f8efda5659
z-vyytx <dobeender@gmail.com>
2016-10-19 16:13:20 +0800

    Update simpleJS.js

--- simpleJS.js
+++ simpleJS.js
@@ -1,7 +1,7 @@
 var imported1 = document.createElement('script');
 var imported2 = document.createElement('script');
 imported1.src = 'https://vyytx.github.io/simple_js/sj.js';
-imported2.src = 'https://vyytx.githuib.io/jquery.js'
+imported2.src = 'https://vyytx.github.io/jquery.js';
 document.head.appendChild(imported2);
 document.head.appendChild(imported1);
 
