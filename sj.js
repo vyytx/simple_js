1cdf89db29d70c34c771f4bf1c0b5f1dba5775e8
z-vyytx <dobeender@gmail.com>
2016-12-24 13:48:48 +0800

    Update sj.js

--- sj.js
+++ sj.js
@@ -6,34 +6,6 @@ var SJ={
     sjBol: "Boolean"
 }
 
-var errAlert = {
-    say: "The correct instruction is 'sj.say(String);'",
-    ques: "The correct instruction is 'sj.ques(String);'",
-    conf: "The correct instruction is 'sj.conf(String);'",
-    converter:{
-        txtNum: "The correct instruction is 'sj.converter.txtNum(String);'",
-        numTxt: "The correct instruction is 'sj.converter.numTxt(Number);'",
-        txtBol: "The correct instruction is 'sj.converter.txtBol(String);'",
-        bolTxt: "The correct instruction is 'sj.converter.bolTxt(Boolean);'",
-        numBol: "The correct instruction is 'sj.converter.numBol(Number);'",
-        bolNum: "The correct instruction is 'sj.converter.bolNum(Boolean);'"
-    }
-}
-
-function jsHelp (txt){
-    switch(txt){
-        case "say":
-            alert(errAlert.say);
-            break;
-        case "say":
-            alert(errAlert.say);
-            break;
-        default:
-            alert('Unmatch!!');
-            break;
-    }
-}
-
 var sj = {
     say: function(txt){alert(txt)},
     ques: function(txt){prompt(txt)},
